37b371c42aaa17a07274031f1a8088007e035803
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-05-22 14:57:05 +0200

    Made changes in Game and Tournament classes

--- GameTournament.Core/Entities/Game.cs
+++ GameTournament.Core/Entities/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ namespace GameTournament.Core.Entities
 {
     public class Game
     {
+        [Key]
         public int Id { get; set; }
-        public string GameTitle { get; set; }
+
+        [Required]
+        public string? GameTitle { get; set; }
         public DateTime Time { get; set; }
         public int TournamentId { get; set; }
-        public Tournament Tournament { get; set; }
+        public Tournament? Tournament { get; set; }
     }
 }
--- GameTournament.Core/Entities/Tournament.cs
+++ GameTournament.Core/Entities/Tournament.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,17 @@ namespace GameTournament.Core.Entities
 {
     public class Tournament
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required, StringLength(20, MinimumLength =3, ErrorMessage ="Must contain 3 - 20 letters")]
         public string TournamentTitle { get; set; }
         public DateTime StartDate { get; set; }
-
-        public ICollection<Game> Games { get; set; }
+        public ICollection<Game>? Games { get; set; }
+        
+        public Tournament() 
+        {
+            Games = new List<Game>();
+        }
     }
 }
