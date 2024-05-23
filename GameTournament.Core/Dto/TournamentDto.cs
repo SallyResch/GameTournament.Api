670e82c8d41f16039ebde089ec032e9b70633bcd
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-05-23 16:34:37 +0200

    Adde DTO:s and NugetPackages DependencyInjection

---
+++ GameTournament.Core/Dto/TournamentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTournament.Core.Dto
+{
+    internal class TournamentDto
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public TournamentDto() 
+        {
+            StartDate = DateTime.Now;
+            EndDate = StartDate.AddMonths(3).Date;
+        }
+    }
+}
