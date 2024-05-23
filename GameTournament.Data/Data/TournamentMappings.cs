475aa91662e6110ae1064d05e1502273942a7869
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-05-23 16:38:35 +0200

    Added TournamentMappings class in Data-Project

---
+++ GameTournament.Data/Data/TournamentMappings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTournament.Data.Data
+{
+    internal class TournamentMappings
+    {
+    }
+}
